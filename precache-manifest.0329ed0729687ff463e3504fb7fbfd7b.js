6616a9d445119a675dcf31ebfb920173631dffc2
Piotr Kolaniak <piokol807@interia.pl>
2020-10-05 10:16:39 +0200

    Updates

---
+++ precache-manifest.0329ed0729687ff463e3504fb7fbfd7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c516b95971136c54bf88addfec7d7b18",
+    "url": "/react-crypto-rate/index.html"
+  },
+  {
+    "revision": "46cdf725c04aeefbeaaf",
+    "url": "/react-crypto-rate/static/css/main.4ad59f47.chunk.css"
+  },
+  {
+    "revision": "e3196a1673ee8543f9b2",
+    "url": "/react-crypto-rate/static/js/2.9b905bb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-crypto-rate/static/js/2.9b905bb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46cdf725c04aeefbeaaf",
+    "url": "/react-crypto-rate/static/js/main.8de73d58.chunk.js"
+  },
+  {
+    "revision": "b5c19fbaddd7036d5329",
+    "url": "/react-crypto-rate/static/js/runtime-main.075283e4.js"
+  },
+  {
+    "revision": "d7876d48e80112e766d737346a616707",
+    "url": "/react-crypto-rate/static/media/logo.d7876d48.svg"
+  }
+]);
